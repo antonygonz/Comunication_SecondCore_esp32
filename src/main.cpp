a4d5835930f46e79d06430a3654a1d0522e9e58a
Antonio Gonzalez <antonygonz266@gmail.com>
2023-06-09 14:48:38 -0600

    primer comit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+String inputString = "";      // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+/*
+TaskHandle_t Task1;
+void loop2(void *parameter){
+  for(;;){
+
+  }
+  vTaskDelay(10);
+}*/
+//
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  /*xTaskCreatePinnedToCore(
+    loop2,
+    "Task_1",
+    1000,
+    NULL,
+    1,
+    &Task1,
+    0
+  );
+  */
+  Serial.begin(9600);//Inicializa la comunicacion
+  inputString.reserve(200);//recerba hasta 200 caracteres en la memoria para los strings de entrada
+  pinMode(LED_BUILTIN, OUTPUT);//inicializa el led
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (stringComplete) {//entra si hay un string completo
+    if (inputString.equals("ON")){
+      digitalWrite(LED_BUILTIN, HIGH);  
+    }else if (inputString=="OFF"){
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    Serial.println(inputString);//Escribe en la consola el ultimo string
+    inputString = "";// Borra el string:
+    stringComplete = false;//Resetea la recepcion
+  }
+
+
+
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      inputString.remove(inputString.lastIndexOf("\n"));//Elimina el caracter salto de linea
+      inputString.remove(inputString.lastIndexOf("\r"));//Elimina el caracter Carriage return
+      stringComplete = true;
+      
+    }
+  }
 }
