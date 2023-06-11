c710fd02840d46cfcb223c20a1b32a58ec847299
Antonio Gonzalez <antonygonz266@gmail.com>
2023-06-11 14:05:00 -0600

    implementacion bme680

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
-
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
 // Variables de la comunicacion
 String inputString = "";      // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
@@ -38,6 +44,16 @@ float previous_error = 0;
 float PID_value = 0;
 float Error_INT = 0;
 
+Adafruit_BME680 bme;
+float temp = 20;
+/*//variables del servidor
+const char* ssid = "XXX";
+const char* password = "XXX";
+const char* mqtt_server = "XXX";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+*/
 void InterrupcionCruceZero(){
   detectado=1;
 }
@@ -108,9 +124,10 @@ void loop2(void *parameter){
   
 }
 //
-
+unsigned long lastMsg=0;
 void setup() {
-  Serial.begin(115200);//Inicializa la comunicacion
+  delay(100);
+  Serial.begin(9600);//Inicializa la comunicacion
   inputString.reserve(200);//recerba hasta 200 caracteres en la memoria para los strings de entrada
   pinMode(LED_BUILTIN, OUTPUT);//inicializa el led
   
@@ -118,12 +135,54 @@ void setup() {
   pinMode (pin_cruce_cero,INPUT);
   attachInterrupt(pin_cruce_cero,InterrupcionCruceZero,RISING);
 
+  
+  //while (!Serial);
+
+  if (!bme.begin()) {
+    while (!bme.begin())
+    {
+      delay(100);
+      Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    }
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
   /*
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqtt_server, 1883);
   */
   
 }
 
 void loop() {
+  /*if (!client.connected()) {
+    reconnect();
+  }
+
+  StaticJsonDocument<80> doc;
+  char output[80];
+  */
   Tiempo_Actual_loop1=micros();
   if (stringComplete) {//entra si hay un string completo
     Serial.println(inputString);//Escribe en la consola el ultimo string
@@ -132,7 +191,7 @@ void loop() {
     }else if (inputString=="OFF"){
       digitalWrite(LED_BUILTIN, LOW);
     }else if (inputString=="TEMP"){
-      Serial.println(String(Temperatura));
+      Serial.println(String(temp));
     }else if (inputString=="OLSTART"){
       //vTaskSuspend(Task1);
       xTaskCreatePinnedToCore(loop2,"Task_1",2000,NULL,1,/*intenta cambiar prioridad a 0, original 1*/&Task1,0);
@@ -146,6 +205,15 @@ void loop() {
     inputString = "";// Borra el string:
     stringComplete = false;//Resetea la recepcion
   }
+  if (Tiempo_Actual_loop1-Tiempo_Previo_loop1>=100)
+  {
+    Tiempo_Previo_loop1=Tiempo_Actual_loop1;
+    temp = bme.readTemperature();
+    Temperatura=temp;
+    Serial.println(temp);
+  }
+  
+  /*
   if ((Tiempo_Actual_loop1-Tiempo_Previo_loop1)>=100&&contadorFiltro<=1000)
   {
     TempFiltro=TempFiltro+analogReadMilliVolts(SensorLM35);
@@ -159,13 +227,32 @@ void loop() {
     contadorFiltro=0;
     Tiempo_Previo_loop1=Tiempo_Actual_loop1;
     //Serial.println(Temperatura);
+    */
+  
+    unsigned long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    //float temp = bme.readTemperature();
+    float pressure = bme.readPressure()/100.0;
+    float humidity = bme.readHumidity();
+    float gas = bme.readGas()/1000.0;
+    /*doc["t"] = temp;
+    doc["p"] = pressure;
+    doc["h"] = humidity;
+    doc["g"] = gas;
+
+    serializeJson(doc, output);
+    Serial.println(output);
+    client.publish("/home/sensors", output);*/
+  }
   }
   
   
+  
 
 
 
-}
+
 
 /*
   SerialEvent occurs whenever a new data comes in the hardware serial RX. This
